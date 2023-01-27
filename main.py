bbe56496096010422489530580d5ac985e2fc54c
Vicente Aguilera Arias <vaguiler@emeal.nttdata.com>
2023-01-27 10:07:00 -0300

    Update main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import socket
 
 #parametros
 width, height = 1280, 720
@@ -12,15 +13,25 @@ cap.set(4, height)
 #detector de manos
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
+#comunicacion
+com = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverAddress = ('127.0.0.1', 5052)
+
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
     hands, img = detector.findHands(img)
+    dataFingers = []
 
     if hands:
         hand = hands[0]
         fingers = detector.fingersUp(hand)
-        print(fingers)
+        #print(fingers)
+        lmString = ""
+        dataFingers = fingers
+        print(dataFingers)
+
+    com.sendto(str.encode(str(dataFingers)), serverAddress)
 
     cv2.imshow("Video", img)
     key = cv2.waitKey(1)
